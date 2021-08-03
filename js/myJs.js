15a6697bb31bdd41c852c330e133a844f88dc80a
2imKang <kangt18@kakao.com>
2021-08-04 00:04:14 +0900

    update

--- js/myJs.js
+++ js/myJs.js
@@ -14,9 +14,9 @@ function firstQuestion(){
 
     $('.content').hide();
     Swal.fire({
-        title: 'Chào emmm!',
+        title: 'Chào em, Anh tên là Cường(Kang)!',
         text: 'Anh có điều này muốn hỏi, em hãy trả lời thật lòng nha',
-        imageUrl: '../img/cuteCat.jpg',
+        imageUrl: '../img/1.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("../img/iput-bg.jpg")',
